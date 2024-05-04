ddbc6f43ded2a961e017394f0f2975135e4243a4
Peace-lov <chidieberepeace42@.com>
2024-05-04 08:38:57 -0700

    web dynamic

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,41 +1,46 @@
 #!/usr/bin/python3
-"""Module holds the major application"""
+"""Module holds the flask application"""
 from models import storage
 from api.vI.views import app_views
-from flask import Flask, make_response, jsonify
-from os import getenv
+from flask import Flask, make_response, jsonify, render_template
+from os import environ
 from flask_cors import CORS
 from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+# cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.teardown_appcontent
-def close_db(obj):
-    """calls the mehtod close() on storage"""
+@app.teardown_appcontext
+def close_db(error):
+    """ calls the mehtod close() on storage """
     storage.close()
 
 
-@app.erorhandle(404)
+@app.errorhandler(404)
 def page_not_found(error):
-    """Looks for customised page not found"""
+    """ Looks for customised page not found """
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
 app.config['SWAGGER'] = {
     'title': 'AirBnB clone - RESTful API',
-    'description': 'This api was created to handle HBNB project,\
-    Below are all the documentation',
-    'universion': 3}
+    'universion': 3
+}
 
 Swagger(app)
 
 if __name__ == "__main__":
+    """ This is the main function """
 
-    host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = getenv('HBNB_API_PORT', default=5000)
-
-    app.run(host, int(port), threaded=True)
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
